3adddbd019b7353b1bae8c84093fbe928985ca3b
Nrgn <mejumine@gmail.com>
2023-02-04 16:02:11 +0100

    rien de rien

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: romaurel <romaurel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/04 10:43:44 by romaurel          #+#    #+#             */
-/*   Updated: 2023/02/04 11:09:08 by romaurel         ###   ########.fr       */
+/*   Updated: 2023/02/04 14:49:28 by romaurel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 char	*get_next_line(int fd)
 {
 	static char	buffer[1024];
+	ssize_t	bytes_read;
 
-	
+	bytes_read = read(fd, buffer, sizeof(buffer));
+
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,16 @@
 /*   By: romaurel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/04 10:24:18 by romaurel          #+#    #+#             */
-/*   Updated: 2023/02/04 10:25:53 by romaurel         ###   ########.fr       */
+/*   Updated: 2023/02/04 14:09:44 by romaurel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
 
 char	*get_next_line(int fd);
 
