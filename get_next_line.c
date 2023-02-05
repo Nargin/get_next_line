48bee9686b522e755804a4ff0f526bfbd73c5814
Robin Maurell <romaurel@c3r5p2.42perpignan.fr>
2023-02-05 22:52:54 +0100

    fini le

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: romaurel <romaurel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/04 10:43:44 by romaurel          #+#    #+#             */
-/*   Updated: 2023/02/05 16:10:56 by romaurel         ###   ########.fr       */
+/*   Updated: 2023/02/05 22:23:43 by romaurel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,10 @@ char	*ft_strjoin(char *s1, char *s2)
 	char	*nx;
 
 	if (!s1)
-		return (s2);
-	if (!s2)
-		return (s1);
-	len = strlenn(s1, 1) + strlenn(s2, 1);
+		len = strlenn(s2, 1);
+	else
+		len = strlenn(s1, 1) + strlenn(s2, 1);
+	printf("len : %d\n", len);
 	nx = malloc((len + 1) * sizeof(char));
 	if (!nx)
 		return (0);
@@ -66,13 +66,14 @@ char	*reader(char *buffer, int fd)
 {
 	char	*temp;
 	int	rfl;
+	int i = 0;
 
 	rfl = 1;
 	temp = malloc(BUFFER_SIZE + 1);
-	while (!ft_strchr(buffer, '\n') && rfl)
+	while (!ft_strchr(temp, '\n') && rfl)
 	{
 		rfl = read(fd, temp, BUFFER_SIZE);
-		printf("%s\n", temp);
+		printf("carac : %d, lettre -> %s\n", i++, temp);
 		buffer = ft_strjoin(buffer, temp);
 	}
 	free(temp);
@@ -84,10 +85,10 @@ char	*get_next_line(int fd)
 	static char	*buffer;
 	char	*tmp;
 
-	if (!fd || !read(fd, 0, 0) || !BUFFER_SIZE)
+	if (fd == -1 || !BUFFER_SIZE)
 		return (0);
 	buffer = reader(buffer, fd);
-	printf("%s\n", buffer);
+	printf("buffer : %s\n", buffer);
 	return (buffer);
 }
 
@@ -96,6 +97,6 @@ int	main(void)
 	int fd = open("text.txt", O_RDONLY);
 
 	printf("fd : %d\n", fd);
-	get_next_line(fd);
+	printf("line : %s", get_next_line(fd));
 	return (0);
 }
