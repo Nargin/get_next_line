4e0b903829b2a56ee935b3f4a97348bbac68c2ec
Robin Maurell <romaurel@c3r5p2.42perpignan.fr>
2023-02-07 17:26:20 +0100

    segfault

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: romaurel <romaurel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/04 10:43:44 by romaurel          #+#    #+#             */
-/*   Updated: 2023/02/06 15:07:40 by romaurel         ###   ########.fr       */
+/*   Updated: 2023/02/07 17:09:15 by romaurel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,6 @@ char	*ft_strjoin(char *s1, char *s2)
 	while (s1[k] && i < len)
 		nx[i++] = s1[k++];
 	k = 0;
-	printf("len : %d\n", i);
 	while (s2[k] && i < len)
 		nx[i++] = s2[k++];
 	nx[i] = 0;
@@ -51,15 +50,14 @@ int	strlenn(char *s, int option)
 	int	i;
 
 	i = 0;
-	if (!*s)
+	if (!s)
 		return (0);
 	if (option == 1)
 		while (s[i])
 			i++;
 	else
-		while (s[i] || s[i] != '\n')
+		while (s[i] && s[i] != '\n')
 			i++;
-	printf("strlenn -> i : %d\n", i);
 	return (i);
 }
 
@@ -69,27 +67,25 @@ char	*reader(char *buffer, int fd)
 	int	rfl;
 
 	rfl = 1;
-	temp = malloc(BUFFER_SIZE + 1);
+	temp = malloc(BUFFER_SIZE);
+	if (!temp)
+		return (0);
 	while (!ft_strchr(temp, '\n') && rfl)
 	{
 		rfl = read(fd, temp, BUFFER_SIZE);
-		printf("lettre -> %s\n", temp);
 		buffer = ft_strjoin(buffer, temp);
-		free(temp);
 	}
-	printf("test\n");
+	free(temp);
 	return (buffer);
 }
 
 char	*get_next_line(int fd)
 {
 	static char	*buffer;
-	char	*tmp;
 
-	if (fd == -1 || !BUFFER_SIZE || !read(fd, 0, 0) == -1)
+	if (fd == -1 || !BUFFER_SIZE || read(fd, 0, 0) == -1)
 		return (0);
 	buffer = reader(buffer, fd);
-	printf("buffer : %s\n", buffer);
 	return (buffer);
 }
 
@@ -97,6 +93,7 @@ int	main(void)
 {
 	int fd = open("text.txt", O_RDONLY);
 
-	printf("line : %s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
 	return (0);
 }
