d535621c569641446cf2bd533eb86d8a3631d34c
Robin Maurell <romaurel@c3r5p2.42perpignan.fr>
2023-02-13 17:17:43 +0100

    no NULL return

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: romaurel <romaurel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/04 10:43:44 by romaurel          #+#    #+#             */
-/*   Updated: 2023/02/11 15:10:07 by romaurel         ###   ########.fr       */
+/*   Updated: 2023/02/13 17:17:11 by romaurel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ char	*ft_strjoin(char *s1, char *s2)
 	int		i;
 	int		len;
 
-	if (!s1)
+	if (!s1 && s2)
 	{
 		free(s1);
 		return (ft_strndup(s2, 0, ft_strlen(s2)));
@@ -41,12 +41,9 @@ char	*ft_strjoin(char *s1, char *s2)
 
 char	*reader(char *buffer, int fd)
 {
-	char	*str;
+	char	str[BUFFER_SIZE + 1];
 	int		rfl;
 
-	str = malloc(sizeof(char) * (BUFFER_SIZE + 1));
-	if (!str)
-		return (0);
 	rfl = 1;
 	while (rfl && !ft_strchr(str, '\n'))
 	{
@@ -54,7 +51,6 @@ char	*reader(char *buffer, int fd)
 		str[rfl] = 0;
 		buffer = ft_strjoin(buffer, str);
 	}
-	free(str);
 	return (buffer);
 }
 
@@ -64,17 +60,20 @@ char	*ft_cl(char *str)
 	int		i;
 
 	if (!str)
-		return (0);
+		return ((void)free(str), (char *)NULL);
 	i = 0;
 	while (str[i] && str[i] != '\n')
 		i++;
-	nx = malloc((i + 2) * sizeof(char));
+	if (i == ft_strlen(str) - 1)
+		nx = malloc((i + 1) * sizeof(char));
+	else
+		nx = malloc((i + 2) * sizeof(char));
 	if (!nx)
-		return (0);
+		return ((char *)NULL);
 	i = -1;
 	while (str[++i] && str[i] != '\n')
 		nx[i] = str[i];
-	if (str[i] == '\n' && str[i])
+	if (str[i] == '\n')
 		nx[i++] = '\n';
 	nx[i] = 0;
 	return (nx);
@@ -87,15 +86,12 @@ char	*ft_nl(char *str)
 	int		u;
 
 	if (!str)
-		return (0);
+		return ((char *)NULL);
 	i = 0;
 	while (str[i] && str[i] != '\n')
 		i++;
 	if (!str[i])
-	{
-		free(str);
-		return (0);
-	}
+		return (free(str), (char *)NULL);
 	nx = malloc(sizeof(char) * (ft_strlen(str) - i + 1));
 	if (!nx)
 		return (0);
@@ -114,25 +110,34 @@ char	*get_next_line(int fd)
 
 	if (BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0 || fd < 0)
 		return (0);
-	buffer = reader(buffer, fd);
+	if (!ft_strchr(buffer, '\n'))
+		buffer = reader(buffer, fd);
 	if (!buffer)
-		return (NULL);
+		buffer = NULL;
+	printf("ft_cl : %s avec %s.\n", ft_cl(buffer), buffer);
 	line = ft_cl(buffer);
+	// printf("line before return : %d", line == NULL ? 1 : 0);
 	buffer = ft_nl(buffer);
 	return (line);
 }
-/*
+
 int	main(void)
 {
 	int fd = open("text.txt", O_RDONLY);
+	char	*str;
+	// int i = 0;
 
-	// printf("0 : %s", get_next_line(fd));
-	// printf("1 : %s", get_next_line(fd));
-	// printf("2 : %s", get_next_line(fd));
-	// printf("3 : %s", get_next_line(fd));
-	// printf("4 : %s", get_next_line(fd));
-	for (int i = 0; i < 10; i++)
-		printf("%s", get_next_line(fd));
-		// get_next_line(fd);
+	if (0 == NULL)
+		printf("gnl : \n");
+	str = get_next_line(fd);
+	while (str)
+	{
+		printf(" - %s", str);
+		free(str);
+		str = get_next_line(fd);
+		// if (i++ == 5)
+		// 	str = 0;
+	}
+	printf("%p\n", str);
 	return (0);
-}*/
+}
