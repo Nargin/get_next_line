7d15a8b5157c7cbf6ff8a5075b884761449676dd
Robin Maurell <romaurel@c3r5p2.42perpignan.fr>
2023-02-15 15:50:26 +0100

    all ok + bonus ok

---
+++ get_next_line_bonus.h
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: romaurel <romaurel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/15 15:46:46 by romaurel          #+#    #+#             */
+/*   Updated: 2023/02/15 15:46:47 by romaurel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1024
+# endif
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <string.h>
+
+char	*get_next_line(int fd);
+
+char	*reader(char *buffer, int fd);
+
+int		ft_strlen(char *s);
+
+int		ft_strchr(char *s, char c);
+
+char	*ft_strjoin(char *s1, char *s2);
+
+char	*ft_cl(char **buffer);
+
+char	*ft_strndup(char *s, int len);
+
+char	*soloq_lp_farmer(char **buffer);
+
+#endif
